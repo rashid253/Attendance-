2f9b989ac892a8f06833fbb0d927a3051ea12941
rashid253 <rashidncit@gmail.com>
2025-05-08 00:15:47 +0500

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ window.addEventListener('DOMContentLoaded', async () => {
       font-weight: bold;
       font-size: 1.5rem;
       color: royalblue;
-      font-family: 'Bubblegum Sans', cursive;
+      font-family: 'Poppins', cursive;
     }
     .attendance-student-container {
       margin-bottom: 1rem;
