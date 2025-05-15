f5121dd4b4f4833fa7bdfdf1fc624983c3f119e3
rashid253 <rashidncit@gmail.com>
2025-05-15 09:55:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1000,7 +1000,6 @@ shareAttendanceBtn.onclick = () => {
   })();
 
   // --- 12. Service Worker ---
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js').catch(console.error);
+ navigator.serviceWorker.register('/sw.js').catch(console.error);
   }
 });
