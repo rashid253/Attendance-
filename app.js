7ef37f5d49250f24bb628ba4232a51ce4a0b99c8
rashid253 <rashidncit@gmail.com>
2025-05-16 15:48:35 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 // app.js
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
+
+// Apna config yahan paste karein
+const firebaseConfig = {
+  apiKey: "AIzaSyBsx5pWhYGh1bJ9gL2bmC68gVc6EpICEzA",
+  authDomain: "attandace-management.firebaseapp.com",
+  projectId: "attandace-management",
+  storageBucket: "attandace-management.firebasestorage.app",
+  messagingSenderId: "222685278846",
+  appId: "1:222685278846:web:aa3e37a42b76befb6f5e2f",
+  measurementId: "G-V2MY85R73B"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+const db = getFirestore();
+console.log("✅ Firebase initialized");
 window.addEventListener('DOMContentLoaded', async () => {
   // --- Universal PDF share helper (must come first) ---
   async function sharePdf(blob, fileName, title) {
