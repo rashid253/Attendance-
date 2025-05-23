a17e9f8c79919db11412947f35240781850de94f
rashid253 <rashidncit@gmail.com>
2025-05-23 17:43:14 +0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import {
+  getDatabase,
+  ref as dbRef,
+  set as dbSet,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsx…EpICEzA",
+  authDomain: "attandace-management.firebaseapp.com",
+  projectId: "attandace-management",
+  storageBucket: "attandace-management.appspot.com",
+  messagingSenderId: "222685278846",
+  appId: "1:222685278846:web:aa3e37a42b76befb6f5e2f",
+  measurementId: "G-V2MY85R73B",
+  databaseURL: "https://attandace-management-default-rtdb.firebaseio.com",
+};
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const appDataRef = dbRef(database, "appData");
