030f3dc757088d2d03863d86e6cad90d0ef87bbd
rashid253 <rashidncit@gmail.com>
2025-06-04 00:51:24 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,27 +1,49 @@
 // firebase-config.js
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js';
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+  onValue,
+  push,
+  child,
+  remove
+} from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js';
 
-// اپنی Firebase پروجیکٹ کی تفصیلات یہاں درج کریں:
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBsx5pWhYGh1bJ9gL2bmC68gVc6EpICEzA",
+  authDomain: "attandace-management.firebaseapp.com",
+  databaseURL: "https://attandace-management-default-rtdb.firebaseio.com",
+  projectId: "attandace-management",
+  storageBucket: "attandace-management.firebasestorage.app",
+  messagingSenderId: "222685278846",
+  appId: "1:222685278846:web:aa3e37a42b76befb6f5e2f",
+  measurementId: "G-V2MY85R73B"
 };
 
-// Firebase ایپ initialize کریں
-const app = initializeApp(firebaseConfig);
-
-// Authentication کے لیے instance بنائیں
-const auth = getAuth(app);
-
-// Realtime Database کے لیے instance بنائیں
+const app      = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+const auth     = getAuth(app);
 
-export { auth, database };
+const dbRef     = ref;
+const dbSet     = set;
+const dbGet     = get;
+const dbOnValue = onValue;
+const dbPush    = push;
+const dbChild   = child;
+const dbRemove  = remove;
+
+export {
+  auth,
+  database,
+  dbRef,
+  dbSet,
+  dbGet,
+  dbOnValue,
+  dbPush,
+  dbChild,
+  dbRemove
+};
